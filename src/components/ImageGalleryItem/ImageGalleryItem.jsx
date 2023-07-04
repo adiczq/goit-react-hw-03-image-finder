8a88baca28a2a129334519814800526c1de522d5
Adrian Lacheta <adiczq@gmail.com>
2023-07-04 10:08:34 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
 class ImageGalleryItem extends Component {
+  handleClick = () => {
+    const { onClick, image } = this.props;
+    if (onClick) {
+      onClick(image);
+    }
+  };
+
   render() {
-    const { image, onClick } = this.props;
+    const { image } = this.props;
     return (
-      <li className={css.ImageGalleryItem} onClick={onClick}>
+      <li className={css.ImageGalleryItem} onClick={this.handleClick}>
         <img src={image.webformatURL} alt={image.tags} />
       </li>
     );
@@ -16,6 +23,7 @@ class ImageGalleryItem extends Component {
 ImageGalleryItem.propTypes = {
   image: PropTypes.shape({
     webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
     tags: PropTypes.string.isRequired,
   }).isRequired,
   onClick: PropTypes.func.isRequired,
